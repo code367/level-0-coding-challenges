b4e4a9186ada8308830d1fcbb32e94ce679a31ff
Thabang <tsmodisaotsile@gmail.com>
2021-07-12 16:06:41 +0200

    Corrected the formula for calculating the area of a triangle

--- task-0.5.js
+++ task-0.5.js
@@ -1,5 +1,6 @@
 function calculateTriangleArea(a, b, c){
-  let s = 0.5 * (a + b + c); 
+  let s = (1/2) * (a + b + c);
   return Math.sqrt(s * (s - a) * (s - b) * (s - c));
 }
 
+console.log(calculateTriangleArea(3,4,5));
